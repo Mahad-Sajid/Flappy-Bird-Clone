4608c08572de523c84f51eb8c498931a7c45eb8b
Mahad-Sajid <l250644@lhr.nu.edu.pk>
2025-12-09 12:00:00 +0500

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Audio/SoundBuffer.hpp>
+using namespace sf;
+
+// load textures, fonts
+void Graphics();
+
+// menu
+void drawmenu(RenderWindow& window, int menuselect);
+
+// backgrounds
+void drawbackground(RenderWindow& window);
+void drawground(RenderWindow& window);
+
+// bird & pipes
+void drawbird(RenderWindow& window, int birdchoice);
+void drawpipes(RenderWindow& window);
+
+// score
+void drawscore(RenderWindow& window, int score);
+
+// pause, settings, game over
+void drawpause(RenderWindow& window);
+void drawsettings(RenderWindow& window, int settingsSelect, int birdChoice);
+void drawgameover(RenderWindow& window, int score);
+
+// high scores screen
+void drawhighscores(RenderWindow& window);
+
+// load game screen
+void drawloadgame(RenderWindow& window, int selection);
+
+//for audios
+void playFlapSound();
+void playHitSound();
+void playPointSound();
